d091eaed0c2c54e7189eac364fc7d2b34488bff6
amado1992 <aramirezamdo1992@gmail.com>
2025-06-20 20:49:15 -0400

    Add improvements.

--- MigrateExcelToDatabase.py
+++ MigrateExcelToDatabase.py
@@ -3,10 +3,11 @@ import mysql.connector
 import numpy as np
 
 # Ruta del archivo Excel
-file_path = "D:\\news-projects\\Documentos\\lentes.xlsx"
+file_path = "D:\\news-projects\\Documentos\\lentes2.xlsx"
 
 # Leer el archivo Excel
-df = pd.read_excel(file_path, header=1)
+#df = pd.read_excel(file_path, header=1)
+df = pd.read_excel(file_path, header=0)
 
 # Reemplaza los valores `nan` con `None`
 df = df.replace({np.nan: None})
@@ -47,19 +48,59 @@ table_name = "products"
 
 for index, row in df.iterrows():
     code = create_unique_code('P', index)
+    dchain_spec_status = 0
+    material_group = ""
+    sub_franchise = ""
+    old_material_number = ""
+    material = 0
+    material_description = ""
+    description = ""
+    ean_upc = 0
     adn = 0
     mty = 0
     cd_mx = 0
-    if row['adn'] is not None:
-        adn = row['adn']
-    if row['mty'] is not None:
-        mty = row['mty']
-    if row['cd_mx'] is not None:
-        cd_mx = row['cd_mx']
+
+    if 'dchain_spec_status' in df.columns:
+        if row['dchain_spec_status'] is not None:
+            dchain_spec_status = row['dchain_spec_status']
+
+    if 'material_group' in df.columns:
+        material_group = row['material_group']
+
+    if 'sub_franchise' in df.columns:
+        sub_franchise = row['sub_franchise']
+
+    if 'old_material_number' in df.columns:
+        old_material_number = row['old_material_number']
+
+    if 'material' in df.columns:
+        material = row['material']
+
+    if 'material_description' in df.columns:
+        material_description = row['material_description']
+
+    if 'description' in df.columns:
+        description = row['description']
+
+    if 'ean_upc' in df.columns:
+        ean_upc = row['ean_upc']
+
+    if 'adn' in df.columns:
+        if row['adn'] is not None:
+           adn = row['adn']
+
+    if 'mty' in df.columns:
+       if row['mty'] is not None:
+          mty = row['mty']
+
+    if 'cd_mx' in df.columns:
+       if row['cd_mx'] is not None:
+          cd_mx = row['cd_mx']
+
     query = f"INSERT INTO {table_name} (number, purchase_price, sale_price, taxes, amount, minimum_amount, category, catalogue_id, unit_measurement_id, dchain_spec_status, material_group, sub_franchise, old_material_number, material, material_description, description, ean_upc, adn, mty, cd_mx, url_image, created_at, updated_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    values = (code, 0.00, 0.00, "", 0.00, 0.00, "", None, None, row['dchain_spec_status'], row['material_group'],
-              row['sub_franchise'], row['old_material_number'], row['material'], row['material_description'],
-              row['description'], row['ean_upc'], adn, mty, cd_mx, "", '2025-03-04 13:27:29',
+    values = (code, 0.00, 0.00, "", 0.00, 0.00, "", None, None, dchain_spec_status, material_group,
+              sub_franchise, old_material_number, material, material_description,
+              description, ean_upc, adn, mty, cd_mx, "", '2025-03-04 13:27:29',
               '2025-03-04 13:27:29')
     cursor.execute(query, values)
 
